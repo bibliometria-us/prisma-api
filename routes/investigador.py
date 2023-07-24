2216a45ed1a2b68aef1539cd21ceeff9dd21de3b
Pablo Pazo Jiménez <ppazo@us.es>
2023-07-24 14:34:52 +0200

    feat: resumen investigador

--- routes/investigador.py
+++ routes/investigador.py
@@ -12,6 +12,89 @@ global_responses = gconfig.responses
 global_params = gconfig.params
 
 
+@investigador_namespace.route('/')
+class ResumenInvestigador(Resource):
+    @investigador_namespace.doc(
+        responses=global_responses,
+
+        produces=['application/json'],
+
+        params={**global_params,
+                'id': {
+                    'name': 'ID',
+                    'description': 'ID del investigador',
+                    'type': 'int',
+                },
+                'todos': {
+                    'name': 'todos',
+                    'description': 'Devolver una lista de todos los investigadores (desactivado por defecto)',
+                    'type': 'bool',
+                    'enum': ["True", "False"]
+                }}
+
+    )
+    def get(self):
+        """Devuelve un resumen de datos de un investigador."""
+        headers = request.headers
+        args = request.args
+        # accept_type = headers.get('Accept', 'application/json')
+        accept_type = 'application/json'
+        # Cargar argumentos de búsqueda
+        api_key = args.get('api_key', None)
+        id = args.get('id', None)
+        todos = True if (args.get('todos', "False").lower()
+                         == "true") else False
+
+        # Comprobar api_key
+        comprobar_api_key(api_key=api_key, namespace=investigador_namespace)
+
+        params = []
+        # Columnas a mostrar
+        columns = ["i.idInvestigador as prisma", "concat(i.apellidos, ', ', i.nombre) as nombre_administrativo",
+                   "orcid.valor as orcid", "dialnet.valor as dialnet", "idus.valor as idus", "researcherid.valor as researcherid",
+                   "scholar.valor as scholar", "scopus.valor as scopus", "sica.valor as sica", "sisius.valor as sisius", "wos.valor as wos"]
+
+        # Left joins para obtener identificadores y datos de otras tablas
+
+        plantilla_ids = " LEFT JOIN (SELECT idInvestigador, valor FROM i_identificador_investigador WHERE tipo = '{0}') as {0} ON {0}.idInvestigador = i.idInvestigador"
+
+        left_joins = [plantilla_ids.format("orcid"),
+                      plantilla_ids.format("dialnet"),
+                      plantilla_ids.format("idus"),
+                      plantilla_ids.format("researcherid"),
+                      plantilla_ids.format("scholar"),
+                      plantilla_ids.format("scopus"),
+                      plantilla_ids.format("sica"),
+                      plantilla_ids.format("sisius"),
+                      plantilla_ids.format("wos")]
+
+        query = f"SELECT {', '.join(columns)} FROM i_investigador_activo i"
+
+        query += f"{' '.join(left_joins)}"
+
+        if not todos:
+            query += " WHERE i.idInvestigador = %s"
+            params.append(id)
+
+        try:
+            db = BaseDatos()
+            data = db.ejecutarConsulta(query, params)
+        except:
+            investigador_namespace.abort(500, 'Error del servidor')
+
+        # Comprobar el tipo de output esperado
+        if accept_type == 'application/json':
+            dict_data = format.dict_from_table(data, "prisma")
+            return jsonify(dict_data)
+
+        elif accept_type == 'text/csv':
+            csv_data = format.format_csv(data)
+            return Response(csv_data, mimetype='text/csv')
+
+        else:
+            investigador_namespace.abort(406, 'Formato de salida no soportado')
+
+
 @investigador_namespace.route('/busqueda')
 class BusquedaInvestigadores(Resource):
     @investigador_namespace.doc(
@@ -76,7 +159,7 @@ class BusquedaInvestigadores(Resource):
 
         })
     def get(self):
-        """Devuelve la lista de investigadores."""
+        """Devuelve una lista de investigadores que cumpla simultáneamente todos los campos de búsqueda utilizados."""
         headers = request.headers
         args = request.args
         accept_type = headers.get('Accept', 'application/json')
@@ -92,16 +175,16 @@ class BusquedaInvestigadores(Resource):
         instituto = args.get('instituto', None)
         centro = args.get('centro', None)
         doctorado = args.get('doctorado', None)
-        activo = True if (args.get('activo', "True") == "True") else False
+        activo = True if (args.get('activo', "True").lower()
+                          == "true") else False
 
         # Comprobar api_key
-        if not (request.referrer and request.referrer.startswith(request.host_url)):
-            if not api_key or not comprobar_api_key(api_key):
-                investigador_namespace.abort(401, 'API Key inválida')
+        comprobar_api_key(api_key=api_key, namespace=investigador_namespace)
 
         conditions = []
         params = []
 
+        # Comprobar qué parámetros de búsqueda están activos y generar filtros para la consulta
         if nombre:
             conditions.append(
                 "nombre COLLATE utf8mb4_general_ci LIKE CONCAT('%', %s, '%')")
@@ -140,13 +223,15 @@ class BusquedaInvestigadores(Resource):
                    "idDepartamento", "idGrupo", "idCentro", "nacionalidad", "sexo", "fechaNombramiento", "fechaActualizacion"]
 
         # Construir la consulta SQL parametrizada
-        query = "SELECT {} FROM i_investigador".format(", ".join(columns))
+        query = f"SELECT {', '.join(columns)} FROM i_investigador"
 
+        # Decidir si se filtra o no por investigadores activos
         if activo:
-            query += " JOIN (SELECT idInvestigador as id FROM i_investigador_activo) a ON i_investigador.idInvestigador = a.id"
+            query += " JOIN (SELECT idInvestigador as id FROM i) a ON i_investigador.idInvestigador = a.id"
 
+        # Concatenar las queries de campos de búsqueda
         if conditions:
-            query += " WHERE {}".format(" AND ".join(conditions))
+            query += f" WHERE {' AND '.join(conditions)}"
 
         try:
             db = BaseDatos()
--- security/api_key.py
+++ security/api_key.py
@@ -1,7 +1,8 @@
 from db.conexion import BaseDatos
+from flask import request
 
 
-def comprobar_api_key(api_key: str) -> bool:
+def buscar_api_key(api_key: str) -> bool:
     db = BaseDatos(database="api")
     query = "SELECT COUNT(*) FROM api_key WHERE api_key = %s"
     params = [api_key]
@@ -11,3 +12,9 @@ def comprobar_api_key(api_key: str) -> bool:
         return True
 
     return False
+
+
+def comprobar_api_key(api_key: str, namespace):
+    if not (request.referrer and request.referrer.startswith(request.host_url)):
+        if not api_key or not buscar_api_key(api_key):
+            namespace.abort(401, 'API Key inválida')
