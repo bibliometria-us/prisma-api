171eba14a0161730b01155f284ab1b62e9beb42d
Unidad de Bibliometria <bibliometria@us.es>
2024-09-03 09:57:41 +0000

    fix

--- celery_config.py
+++ celery_config.py
@@ -1,8 +1,8 @@
 # celery_config.py
 from kombu import Exchange, Queue
 
-CELERY_BROKER_URL = "redis://localhost:6379/0"
-CELERY_RESULT_BACKEND = "redis://localhost:6379/0"
+CELERY_BROKER_URL = "redis://redis:6379/0"
+CELERY_RESULT_BACKEND = "redis://redis:6379/0"
 CELERY_TASK_SERIALIZER = "json"
 CELERY_RESULT_SERIALIZER = "json"
 CELERY_ACCEPT_CONTENT = ["json"]
