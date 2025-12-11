9b7f2416a920dbe08cbf5f917fece9694eb71d41
Francisco J. Macías <fmacias3@us.es>
2025-12-11 14:03:23 +0100

    fix: numero de colas en start.sh

--- start.sh
+++ start.sh
@@ -4,7 +4,8 @@ if [ $DEBUG = 'true' ]
 then
     sleep infinity
 else
-    celery multi start 3 -A app.celery --loglevel=error --concurrency=1 -Q:1 cargas -Q:2 wosjournals -Q:3 informes -Q:4 openalex && \
+    celery -A app.celery purge
+    celery multi start 4 -A app.celery --loglevel=error --concurrency=1 -Q:1 cargas -Q:2 wosjournals -Q:3 informes -Q:4 openalex && \
     gunicorn app:app -b 0.0.0.0:8001 --access-logfile logs/access.log --error-logfile logs/errors.log --timeout 600
 fi
 
